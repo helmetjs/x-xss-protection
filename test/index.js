ca76623a91911b76f522b053995e69d7615bc2f2
Evan Hahn <me@evanhahn.com>
2016-08-07 12:31:42 -0700

    Minor: use `this` context for variables in tests

--- test/index.js
+++ test/index.js
@@ -7,57 +7,59 @@ var each = require('async/each')
 var assert = require('assert')
 
 describe('x-xss-protection', function () {
-  function grabList (filename) {
-    return rfile(filename)
-      .split('\n')
-      .filter(function (line) {
-        return line.trim() !== ''
-      })
-  }
+  before(function () {
+    function grabList (filename) {
+      return rfile(filename)
+        .split('\n')
+        .filter(function (line) {
+          return line.trim() !== ''
+        })
+    }
 
-  var enabledBrowsers = grabList('./enabled_browser_list.txt')
-  var disabledBrowsers = grabList('./disabled_browser_list.txt')
+    this.enabledBrowsers = grabList('./enabled_browser_list.txt')
+    this.disabledBrowsers = grabList('./disabled_browser_list.txt')
+  })
 
-  var app
   beforeEach(function () {
-    app = connect()
-    app.use(xssFilter())
-    app.use(function (req, res) {
+    this.app = connect()
+    this.app.use(xssFilter())
+    this.app.use(function (req, res) {
       res.end('Hello world!')
     })
   })
 
   it('enables it for supported browsers', function (done) {
-    each(enabledBrowsers, function (useragent, callback) {
-      request(app).get('/').set('User-Agent', useragent)
+    each(this.enabledBrowsers, function (useragent, callback) {
+      request(this.app).get('/').set('User-Agent', useragent)
         .expect('X-XSS-Protection', '1; mode=block', callback)
-    }, done)
+    }.bind(this), done)
   })
 
   it('disables it for unsupported browsers', function (done) {
-    each(disabledBrowsers, function (useragent, callback) {
-      request(app).get('/').set('User-Agent', useragent)
+    each(this.disabledBrowsers, function (useragent, callback) {
+      request(this.app).get('/').set('User-Agent', useragent)
         .expect('X-XSS-Protection', '0', callback)
-    }, done)
+    }.bind(this), done)
   })
 
   it('sets header if there is an empty user-agent', function (done) {
-    request(app).get('/').set('User-Agent', '')
+    request(this.app).get('/').set('User-Agent', '')
       .expect('X-XSS-Protection', '1; mode=block', done)
   })
 
   it('sets header if there is no user-agent', function (done) {
-    request(app).get('/').unset('User-Agent')
+    request(this.app).get('/').unset('User-Agent')
       .expect('X-XSS-Protection', '1; mode=block', done)
   })
 
   it('allows you to force the header for unsupported browsers', function (done) {
-    app = connect()
+    var app = connect()
     app.use(xssFilter({ setOnOldIE: true }))
     app.use(function (req, res) {
       res.end('Hello world!')
     })
-    each(disabledBrowsers, function (useragent, callback) {
+
+    each(this.disabledBrowsers, function (useragent, callback) {
       request(app).get('/').set('User-Agent', useragent)
         .expect('X-XSS-Protection', '1; mode=block', callback)
     }, done)
